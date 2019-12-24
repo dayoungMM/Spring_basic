5146b77ff1ff0bf7ecec505c02e767898bd169f2
dayoungMM <dalbeetm@gmail.com>
2019-12-24 14:44:12 +0900

    add login page

--- src/main/java/com/multicampus/basic/controller/LoginController.java
+++ src/main/java/com/multicampus/basic/controller/LoginController.java
@@ -1,31 +1,31 @@
 package com.multicampus.basic.controller;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller 
-public class LoginController {
-	@GetMapping("/Login")
-	public String login() {
-		return "login";
-	}
-	@PostMapping("/Login")
-	@ResponseBody
-	public String loginPost(
-			@RequestParam("id") String id,
-			@RequestParam("pw") String pw
-			) {
-		
-		String dbId= "boot";
-		String dbPw = "1234";
-		
-		if(dbId.equals(id) && dbPw.equals(pw)) {
-			return "로그인 성공";
-		}
-		return "로그인 실패";
-	}
-
-}
+//@Controller 
+//public class LoginController {
+//	@GetMapping("/Login")
+//	public String login() {
+//		return "login";
+//	}
+//	@PostMapping("/Login")
+//	@ResponseBody
+//	public String loginPost(
+//			@RequestParam("id") String id,
+//			@RequestParam("pw") String pw
+//			) {
+//		
+//		String dbId= "boot";
+//		String dbPw = "1234";
+//		
+//		if(dbId.equals(id) && dbPw.equals(pw)) {
+//			return "로그인 성공";
+//		}
+//		return "로그인 실패";
+//	}
+//
+//}
